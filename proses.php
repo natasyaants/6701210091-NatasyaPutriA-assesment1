a2e83fa3cf884387e72f28e5d7a5004c02136cd0
natasyaants <natasyaantasari@student.telkomuniversity.ac.id>
2023-03-28 13:19:51 +0700

    v1.3

--- proses.php
+++ proses.php
@@ -1,43 +1,54 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Kalkulator Sederhana</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proses php</title>
 </head>
 <body>
-	<form method="post">
-		<input type="number" name="angka_1" placeholder="Masukkan angka pertama"><br>
-		<input type="number" name="angka_2" placeholder="Masukkan angka kedua"><br>
-		<select name="operator">
-			<option value="tambah">+</option>
-			<option value="kurang">-</option>
-			<option value="kali">*</option>
-			<option value="bagi">/</option>
-		</select><br>
-		<input type="submit" name="submit" value="Hitung">
-	</form>
-	<?php
-		if(isset($_POST['submit'])){
-			$angka1 = $_POST['angka_1'];
-			$angka2 = $_POST['angka_2'];
-			$operator = $_POST['operator'];
+    <table>
+        <tr>
+            <td>
+            <form method="POST" action="">
+                <input type="number" name="angka1" required>
+                <select name="operator">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value=""></option>
+                    <option value="/">/</option>
+                </select>
+                <input type="number" name="angka2" required>
+                <input type="submit" name = "hitung" value="hitung">    
+            </form>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <?php
+                if (isset($_POST["hitung"])){
+                    $angka1 = $_POST['angka1'];
+                    $angka2 = $_POST['angka2'];
+                    $operator = $_POST['operator'];
 
-			switch($operator){
-				case "tambah":
-					$hasil = $angka_1 + $angka_2;
-					break;
-				case "kurang":
-					$hasil = $angka_1 - $angka_2;
-					break;
-				case "kali":
-					$hasil = $angka_1 * $angka_2;
-					break;
-				case "bagi":
-					$hasil = $angka_1 / $angka_2;
-					break;
-			}
+                    if ($operator == '+') {
+                        $hasil = $angka1 + $angka2;
+                    } elseif ($operator == '-') {
+                        $hasil = $angka1 - $angka2;
+                    } elseif ($operator == '*') {
+                        $hasil = $angka1 * $angka2;
+                    } elseif ($operator == '/') {
+                        $hasil = $angka1 / $angka2;
+                    } else {
+                        $hasil = 0;
+                    }
 
-			echo "<br>Hasil perhitungan: ".$hasil;
-		}
-	?>
+                    echo "<h2>Hasil Perhitungan:</h2>";
+                    echo "<p>{$angka1} {$operator} {$angka2} = {$hasil}</p>";
+                }
+                ?>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
